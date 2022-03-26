efdf360dfce9cbe1719a0ebf38d383c8ac78053f
sulfuroussquares <bbetancova@gmail.com>
2022-03-25 20:41:00 -0600

    added image upload capability

--- ios-instagram-clone/ios-instagram-clone/CameraViewController.swift
+++ ios-instagram-clone/ios-instagram-clone/CameraViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -21,6 +22,26 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func onSubmitButton(_ sender: Any) {
+        
+        let post = PFObject(className: "pets")
+        
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current()
+        
+        let imageData = imageView.image?.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { success, error in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("great success")
+            }
+            else {
+                print("error: \(error?.localizedDescription)")
+            }
+        }
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
